5937902a857ff928eb7af999beff55f5d8c47942
weldnor <kolesov.anton.s@gmail.com>
2021-05-01 19:42:24 +0300

    First commit

---
+++ src/test/java/stud/team/pwsbackend/ApplicationTests.java
@@ -0,0 +1,13 @@
+package stud.team.pwsbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
