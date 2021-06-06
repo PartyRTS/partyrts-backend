091f7ded94cecc7a23f762e76f687857762cad27
weldnor <kolesov.anton.s@gmail.com>
2021-06-06 23:32:03 +0300

    Fix test

--- src/main/java/stud/team/pwsbackend/config/FlywayConfig.java
+++ src/main/java/stud/team/pwsbackend/config/FlywayConfig.java
@@ -4,10 +4,12 @@ package stud.team.pwsbackend.config;
 import org.flywaydb.core.Flyway;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 import javax.sql.DataSource;
 
 @Configuration
+@Profile("!test")
 public class FlywayConfig {
 
     @Autowired
